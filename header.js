b95dec99b10d4969e1aab2d66000f188f097864e
jarretthosey <jarrett.hosey3@gmail.com>
2020-11-06 18:56:14 -0600

    spelling change

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 document.write(`<nav class=\"navbar navbar-expand-lg navbar-light bg-light\"\>
-    <a class=\"navbar-brand\" href=\"#\"\>Navbar</a\>
+    <a class=\"navbar-brand\" href=\"index.html\"\>JarrettHosey.dev</a\>
     <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\"\>
       <span class=\"navbar-toggler-icon\"\></span\>
     </button\>
@@ -31,43 +31,4 @@ document.write(`<nav class=\"navbar navbar-expand-lg navbar-light bg-light\"\>
         <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\"\>Search</button\>
       </form\>
     </div\>
-  </nav\>
-  <div class=\"jumbotron jumbotron-fluid\"\>
-    <div class=\"container\"\>
-      <h1 class=\"display-1\"\>Welcome</h1\>
-        <p class=\"lead\"\>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p\>
-    </div\>
-  </div\>
-  <div class='container'\>
-    <div class='row'\>
-  <div class=\"card col-sm\" style=\"width: 18rem;\"\>
-    <div class=\"card-body\"\>
-      <h5 class=\"card-title\"\>Card title</h5\>
-      <h6 class=\"card-subtitle mb-2 text-muted\"\>Card subtitle</h6\>
-      <p class=\"card-text\"\>Some quick example text to build on the card title and make up the bulk of the card's content.</p\>
-      <a href=\"#\" class=\"card-link\"\>Card link</a\>
-      <a href=\"#\" class=\"card-link\"\>Another link</a\>
-    </div\>
-  </div\>
-  
-  <div class=\"card col-sm\" style=\"width: 18rem;\"\>
-    <div class=\"card-body\"\>
-      <h5 class=\"card-title\"\>Card title</h5\>
-      <h6 class=\"card-subtitle mb-2 text-muted\"\>Card subtitle</h6\>
-      <p class=\"card-text\"\>Some quick example text to build on the card title and make up the bulk of the card's content.</p\>
-      <a href=\"#\" class=\"card-link\"\>Card link</a\>
-      <a href=\"#\" class=\"card-link\"\>Another link</a\>
-    </div\>
-  </div\>
-  
-  <div class=\"card col-sm\" style=\"width: 18rem;\"\>
-    <div class=\"card-body\"\>
-      <h5 class=\"card-title\"\>Card title</h5\>
-      <h6 class=\"card-subtitle mb-2 text-muted\"\>Card subtitle</h6\>
-      <p class=\"card-text\"\>Some quick example text to build on the card title and make up the bulk of the card's content.</p\>
-      <a href=\"#\" class=\"card-link\"\>Card link</a\>
-      <a href=\"#\" class=\"card-link\"\>Another link</a\>
-    </div\>
-  </div\>
-</div\>
-</div\>`)
+  </nav\>`)
